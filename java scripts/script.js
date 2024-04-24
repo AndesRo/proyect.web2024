930789af6fcc3b6228ff048e66b596ec419ab2a2
AndresR <129084620+AndesRo@users.noreply.github.com>
2024-04-23 21:04:46 -0400

    Delete java scripts directory

--- java scripts/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('mostrar-formularios').addEventListener('click', function() {
-    var formulariosContainer = document.getElementById('formularios-container');
-    if (formulariosContainer.style.display === 'none') {
-        formulariosContainer.style.display = 'block';
-    } else {
-        formulariosContainer.style.display = 'none';
-    }
-});
